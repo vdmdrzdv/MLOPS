16711e8042f76841f6e183c0ba3868c3d217d077
vdmdrzdv <drovadim@mail.ru>
2024-10-14 23:51:27 +0500

    feat: Уведомление об инференсе в консоль

--- mlops/modeling/predict.py
+++ mlops/modeling/predict.py
@@ -23,6 +23,7 @@ def main(
         if i == 5:
             logger.info("Something happened for iteration 5.")
     logger.success("Inference complete.")
+    print("Успешный инференс!")
     # -----------------------------------------
 
 
